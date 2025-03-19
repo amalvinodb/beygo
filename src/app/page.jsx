f63c2ae6557ea002d0140cc982e24c4493b7a598
amal vinod <amalvinodb2000@gmail.com>
2025-03-19 22:38:21 +0530

    updated the hero image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import "./styles.css";
 
 export default function Home() {
-  return <div style={{ height: "1000px" }}>hello world</div>;
+  return (
+    <div style={{ height: "1000px" }}>
+      <div className="hero-image" style={{ height: "1000px", width: "100%" }}>
+        hello
+      </div>
+    </div>
+  );
 }
